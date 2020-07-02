7e6ff8b4f797aa4bfcac379d5564cfc614555483
chathil <chathil98@gmail.com>
2020-07-02 15:23:52 +0700

    Initial Commit

---
+++ fundamental-sub-one/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  fundamental-sub-one
+//
+//  Created by Abdul Chathil on 7/2/20.
+//  Copyright © 2020 Abdul Chathil. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
